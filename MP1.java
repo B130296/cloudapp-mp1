c76c8ebc819c838500ebf924874c8bef3fda3157
B130296 <b130296@sagitario.cic.ipn.mx>
2015-09-11 20:39:44 -0500

    Final version

--- MP1.java
+++ MP1.java
@@ -3,6 +3,7 @@ import java.lang.reflect.Array;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class MP1 {
     Random generator;
@@ -52,8 +53,64 @@ public class MP1 {
     public String[] process() throws Exception {
         String[] ret = new String[20];
        
-        //TODO
+        //+++++  Begins added code +++++++
+        Scanner sc = new Scanner( new File( this.inputFileName ) );
+        String[] lines = new String[50000];
+        int l=0;
+        while( sc.hasNextLine() ){
+               lines[l] = sc.nextLine().toLowerCase().trim();
+               l++;
+        }
+        
+        Integer[] idxs=new Integer[10000];
+        idxs=getIndexes();
+        HashMap<String, Integer> wc = new HashMap<String, Integer>();
+        List sw=Arrays.asList(stopWordsArray);
+        for( int i: idxs ){
+             StringTokenizer st=new StringTokenizer(lines[i],delimiters,false);
+             while( st.hasMoreTokens() ){
+                    String tk=st.nextToken();
+                    if( ! sw.contains(tk) ){
+                        if( wc.containsKey(tk) )
+                            wc.put( tk, wc.get(tk)+1 );
+                        else
+                            wc.put( tk, 1);
+                    }
+             } 
+        }
+        
+        Set<Entry<String,Integer>> mapEntries = wc.entrySet();
+        List<Entry<String,Integer>> aList = new LinkedList<Entry<String,Integer>>(mapEntries);
 
+        Collections.sort(aList, new Comparator<Entry<String,Integer>>() {
+            @Override
+            public int compare(Entry<String, Integer> ele1,
+                               Entry<String, Integer> ele2) {
+                   if(  ele2.getValue() == ele1.getValue() ){
+                        if( ele2.getKey().compareTo(ele1.getKey())<0 )
+                            return 1;
+                   }
+                   return ele2.getValue().compareTo(ele1.getValue());
+
+            }
+
+        });
+        
+        // Storing the list into Linked HashMap to preserve the order of insertion. 
+        Map<String,Integer> aMap2 = new LinkedHashMap<String, Integer>();
+        for(Entry<String,Integer> entry: aList) {
+            aMap2.put(entry.getKey(), entry.getValue());
+        }
+
+        int x=0;
+        for(Entry<String,Integer> entry : aMap2.entrySet()){
+            ret[x]=entry.getKey();
+            x++;
+            if( x == 20 ){
+                break;
+            }
+        }
+        //+++++  End added code +++++++
         return ret;
     }
 
